5058fa270f33523017f54c680ef805d2ad687cec
katrinewi <katrinew0702@gmail.com>
2020-01-28 11:28:55 +0100

    Hopefully working

--- automate.sh
+++ automate.sh
@@ -2,21 +2,27 @@
 
 mv vuln.csv vuln_"$(date +%Y-%m-%d_%H-%M-%S)".csv
 touch "vuln.csv"
-: > "failed.txt"
+mv failed.txt failed_"$(date +%Y-%m-%d_%H-%M-%S)".txt
+touch "failed.txt"
 
-for image in $(cat ./scraper-files/image-names4.txt)
+for image in $(cat ./scraper-files/image-names6.txt)
 do
     	echo "Adding $image..."
     	success=true
+		counter=$0
     	while true
     	do
+				counter=$(($counter +1))
             	output=$(anchore-cli --url "http://localhost:8228/v1" --u "admin" --p "foobar" image add "$image:latest")
             	echo "$output" | grep "error_code=REGISTRY_PERMISSION_DENIED'}" -q && echo "Permission denied to scan image."&& echo "$image" >> failed.txt && success=false && break
 				echo "$output" | grep "error_code=REGISTRY_IMAGE_NOT_FOUND'}" -q && echo "Image not found."&& echo "$image" >> failed.txt && success=false && break
 				echo "$output" | grep "HTTP Code: 400" -q && echo "Unknown error occured."&& echo "$image" >> failed.txt && success=false && break
-				echo "$output" | grep "Analysis Status: analysis_failed" -q && echo "Analysis failed." && echo "$image" >> failed.txt && success=false && break 
-            	echo "$output" | grep "Analysis Status: analyzed" -q && break || (echo "Not finished, sleeping..." && sleep 20)
-				echo "$output"
+				if [counter -gt 15]
+				then
+						check=$(anchore-cli --url "http://localhost:8228/v1" --u "admin" --p "foobar" image get "$image:latest")
+						echo "$check" | grep "Analysis Status: analysis_failed" -q && echo "Analysis failed."&& echo "$image" >> failed.txt && success=false && break
+				fi
+	         	echo "$output" | grep "Analysis Status: analyzed" -q && break || (echo "Not finished, sleeping..." && sleep 20)
     	done
     	if $success
     	then
