d775ae4418a2246edf41bff26755146504a690ca
Katrine Wist <katrinew0702@gmail.com>
2020-05-21 11:50:34 +0200

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -9,30 +9,29 @@ for image in $(cat ./scraper-files/image-names.txt)
 do
     	echo "Adding $image..."
     	success=true
-		SECONDS=0
-	 	while true
+	SECONDS=0
+	while true
     	do
             	output=$(anchore-cli --url "http://localhost:8228/v1" --u "admin" --p "foobar" image add "$image:latest")
             	echo "$output" | grep "error_code=REGISTRY_PERMISSION_DENIED'}" -q && echo "Permission denied to scan image."&& echo "$image" >> failed.txt && success=false && break
-				echo "$output" | grep "error_code=REGISTRY_IMAGE_NOT_FOUND'}" -q && echo "Image not found."&& echo "$image" >> failed.txt && success=false && break
-				echo "$output" | grep "HTTP Code: 400" -q && echo "Unknown error occured."&& echo "$image" >> failed.txt && success=false && break
-				if [ "$SECONDS" -gt "3600" ]
+		echo "$output" | grep "error_code=REGISTRY_IMAGE_NOT_FOUND'}" -q && echo "Image not found."&& echo "$image" >> failed.txt && success=false && break
+		echo "$output" | grep "HTTP Code: 400" -q && echo "Unknown error occured."&& echo "$image" >> failed.txt && success=false && break
+		if [ "$SECONDS" -gt "3600" ]
                 then
                         echo "Timeout"&& echo "$image" >> failed.txt && success=false && break
                 fi
-				echo "$output" | grep "Analysis Status: analyzed" -q && break || (echo "Not finished, sleeping..." && sleep 20)
-
+		echo "$output" | grep "Analysis Status: analyzed" -q && break || (echo "Not finished, sleeping..." && sleep 20)
 
     	done
     	if $success
     	then
             	echo "Finished, writing result to file..."
             	if [[ $(anchore-cli --url "http://localhost:8228/v1" --u "admin" --p "foobar" image vuln "$image:latest" all) ]]
-				then
-    					anchore-cli --url "http://localhost:8228/v1" --u "admin" --p "foobar" image vuln "$image:latest" all | sed -r 's/,/;/g' |sed -r 's/  +/,/g'| sed 's/,$//' | egrep -v '^Vulnerability ID' | awk '{ print "'"$image"',"$1 }' | awk -F, -v OFS=, '{ if(NF==6) { k=$NF; $6=""; $7=k; print } else { for ( i=NF; i<=7; i++ ) { $i=$i"" } print }}' >> vuln.csv
-				else
-    					echo "No found vulnerabilities"
-				fi
-
+		then
+    			anchore-cli --url "http://localhost:8228/v1" --u "admin" --p "foobar" image vuln "$image:latest" all | sed -r 's/,/;/g' |sed -r 's/  +/,/g'| sed 's/,$//' | egrep -v '^Vulnerability ID' | awk '{ print "'"$image"',"$1 }' | awk -F, -v OFS=, '{ if(NF==6) { k=$NF; $6=""; $7=k; print } else { for ( i=NF; i<=7; i++ ) { $i=$i"" } print }}' >> vuln.csv
+		else
+    			echo "No found vulnerabilities"
 		fi
+
+	fi
 done
