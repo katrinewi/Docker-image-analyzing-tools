bd788c41f4de81cf0b2b0d470a0852eef52e8abf
Katrine Wist <katrinew0702@gmail.com>
2020-05-29 17:05:04 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,63 +2,54 @@ import urllib.request
 from selenium import webdriver
 import time
 
-# specify the url
 urlpage = 'https://hub.docker.com/search/?q=&type=image&page={}'
 page_count = 0
 print("Scraping started")
-image_names = open("./scraper-files/image-names.txt","w")
-image_info = open("./scraper-files/image-info.csv", "w")
+image_names = open("./image-names.txt","w")
+image_info = open("./image-info.csv", "w")
 
 while page_count < 100:
-	page_count +=1
-	url = urlpage.format(page_count)
-	# run firefox webdriver
-	driver = webdriver.Firefox(executable_path = '/home/maltrine/aevolume/Master-thesis-scripts/geckodriver')
-	# get web page
-	driver.get(url)
-	# sleep for 10s, to let the page fully load
-	time.sleep(10)
-	# find elements by xpath
-	results = driver.find_elements_by_xpath("//*[@class='imageSearchResult styles__searchResult___EBKah styles__clickable___2bfia']")
+    page_count +=1
+    url = urlpage.format(page_count)
+    #Run firefox webdriver
+    driver = webdriver.Firefox(executable_path = '/usr/local/Cellar/geckodriver/geckodriver')
+    #Get web page
+    driver.get(url)
+    #Sleep for 10s, to let the page fully load
+    time.sleep(10)
+    #Find elements by xpath
+    results = driver.find_elements_by_xpath("//*[@class='imageSearchResult styles__searchResult___EBKah styles__clickable___2bfia']")
 
-	print('Scraping page: ', page_count)
+    print('Scraping page: ', page_count)
+    for info in results:
+        image_type = ""
+        #Set the image type
+        if('OFFICIAL' in info.text):
+            image_type = "official"
+        elif('Certified' in info.text):
+            image_type = "certified"
+        elif('VERIFIED' in info.text):
+            image_type = "verified"
+        else:
+            image_type = "community"
 
-	for x in results:
-		info = x.text.split('\n')
-		image_type = ""
-		downloads = ""
-		stars = ""
-		#Set image type
-		if('OFFICIAL' in x.text):
-			image_type = "official"
-			downloads = info[1]
-			stars = info[3]
-		elif('VERIFIED' in x.text):
-			image_type = "verified"
-		else:
-			image_type = "community"
-			downloads = info[0]
-			if(info[2].isdigit()):
-				stars = info[2]
-			else:
-				stars = ""
-		image_link = x.get_attribute('href')
-		if(image_type == "official"):
-			tmp = image_link.split("/")[-1] + "," + image_type + "," + downloads  + ","+stars  +"\n"
-			image_names.write(image_link.split("/")[-1]+"\n")
-			image_info.write(tmp)
-		elif(image_type == "community"):
-			tmp =  image_link.split("/")[-2]+"/"+image_link.split("/")[-1]  + "," + image_type + "," + downloads + "," + str(stars)  + "\n"
-			image_names.write(image_link.split("/")[-2]+"/"+image_link.split("/")[-1] +"\n")
-			image_info.write(tmp)
-		else:
-			tmp = image_link.split("/")[-1]+"," + image_type + "," + "," + "\n"
-			image_names.write(image_link.split("/")[-1]+"\n")
-			image_info.write(tmp)
+        #Gather the image name from URL, different procedure for each image type
+        image_link = info.get_attribute('href')
+        if(image_type == "official"):
+            tmp = image_link.split("/")[-1] + "," + image_type + "\n"
+            image_names.write(image_link.split("/")[-1]+"\n")
+            image_info.write(tmp)
+        elif(image_type == "community"):
+            tmp =  image_link.split("/")[-2] + "/" + image_link.split("/")[-1] + "," + image_type + "\n"
+            image_names.write(image_link.split("/")[-2]+"/"+image_link.split("/")[-1] +"\n")
+            image_info.write(tmp)
+        else:
+            tmp = image_link.split("/")[-1] + "," + image_type + "\n"
+            image_names.write(image_link.split("/")[-1]+"\n")
+            image_info.write(tmp)
 
-	time.sleep(5)
-	driver.close()
-
-# close driver
+    time.sleep(5)
+    driver.close()
+    
+#Close driver
 driver.quit()
-
